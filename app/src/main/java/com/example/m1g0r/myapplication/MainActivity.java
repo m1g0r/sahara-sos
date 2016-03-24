3b77a137844230c91b3f456c79c30012632ab0d4
m1g0r <m1g0rm1g0r@gmail.com>
2016-03-24 09:20:57 +0200

    edit CheckBox

--- app/src/main/java/com/example/m1g0r/myapplication/MainActivity.java
+++ app/src/main/java/com/example/m1g0r/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.content.Intent;
 import android.net.Uri;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,23 +16,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-public void facebook(View view) {
-    Intent facebookIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.facebook.com/sahara.com.ua"));
-    startActivity(facebookIntent);
-}
+    public void facebook(View view) {
+        Intent facebookIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.facebook.com/sahara.com.ua"));
+        startActivity(facebookIntent);
+    }
 
-public void twitter(View view) {
-    Intent twitterIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://twitter.com/saharacomua"));
-    startActivity(twitterIntent);
-}
+    public void twitter(View view) {
+        Intent twitterIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://twitter.com/saharacomua"));
+        startActivity(twitterIntent);
+    }
 
-public void instagram(View view) {
-    Intent instagramIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.instagram.com/sahara_smart/"));
-    startActivity(instagramIntent);
-}
+    public void instagram(View view) {
+        Intent instagramIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.instagram.com/sahara_smart/"));
+        startActivity(instagramIntent);
+    }
 
-public void youtube(View view) {
-    Intent youtubeIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.youtube.com/channel/UCbm-QK0tyM4szL8T05HHYvg/videos"));
-    startActivity(youtubeIntent);
-}
+    public void youtube(View view) {
+        Intent youtubeIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.youtube.com/channel/UCbm-QK0tyM4szL8T05HHYvg/videos"));
+        startActivity(youtubeIntent);
+    }
+
 }
